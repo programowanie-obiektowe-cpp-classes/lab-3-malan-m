a09582d994d9c0b56ca880afbebe8d57b0f2c451
Michał Malankiewicz <malankiewicz.m@gmail.com>
2024-11-05 00:02:38 +0100

    rozwiazane

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+if (towar == 0) return 0;
+
+    Stocznia stocznia;
+    unsigned int sumarycznyTransport = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    while (sumarycznyTransport < towar && sumarycznyTransport <= Stocznia::getTotalCap()) {
+        Statek* statek = stocznia();  
+        unsigned int transportowane = statek->transportuj();
+        sumarycznyTransport += transportowane;
+
+        if (dynamic_cast<Zaglowiec*>(statek)) {
+            ++liczbaZaglowcow;
+        }
+
+        delete statek;
+    }
+
+    return liczbaZaglowcow;
 }
